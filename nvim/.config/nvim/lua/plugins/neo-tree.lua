9cd36b10725e5916f91db0fcb800d903a3006b57
Tom Raaff <tomraaff@protonmail.com>
2025-10-24 21:18:44 +0200

    NVIM: USE LAZYVIM

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,48 +1,19 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	version = "*",
-	lazy = false,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	cmd = "Neotree",
-	keys = {
-		{ "<leader>e", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
-	},
-	opts = {
-		filesystem = {
-			window = {
-				mappings = {
-					["<leader>e"] = "close_window",
-					["h"] = "close_node",
-					["l"] = "open",
-				},
-			},
-			filtered_items = {
-				hide_dotfiles = false,
-				hide_gitignored = false,
-			},
-			follow_current_file = { enabled = true },
-			hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-			use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
-		},
-		buffers = {
-			follow_current_file = {
-				enabled = true,
-			},
-		},
-		-- event_handlers = {
-		-- 	{
-		-- 		event = "file_opened",
-		-- 		handler = function()
-		-- 			require("neo-tree.command").execute({ action = "close" })
-		-- 		end,
-		-- 	},
-		-- },
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+          "thumbs.db",
+        },
+        never_show = {},
+      },
+    },
+  },
 }
--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,61 +1,45 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	init = function()
-		require("catppuccin").setup({
-			flavour = "macchiato",
-			background = {
-				light = "latte",
-				dark = "macchiato", -- frappe, macchiato, mocha
-			},
-			transparent_background = false, -- disables setting the background color.
-			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-			dim_inactive = {
-				enabled = true, -- dims the background color of inactive window
-				shade = "dark",
-				percentage = 0.20, -- percentage of the shade to apply to the inactive window
-			},
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				treesitter = true,
-				mini = {
-					enabled = true,
-					indentscope_color = "",
-				},
-				harpoon = true,
-				mason = true,
-				neotree = true,
-				dap = true,
-				dap_ui = true,
-				nvim_surround = true,
-				telescope = {
-					enabled = true,
-				},
-				which_key = true,
-				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-			},
-		})
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  init = function()
+    require("catppuccin").setup({
+      flavour = "macchiato",
+      background = {
+        light = "latte",
+        dark = "macchiato", -- frappe, macchiato, mocha
+      },
+      transparent_background = false, -- disables setting the background color.
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.20, -- percentage of the shade to apply to the inactive window
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+        harpoon = true,
+        mason = true,
+        neotree = true,
+        dap = true,
+        dap_ui = true,
+        nvim_surround = true,
+        telescope = {
+          enabled = true,
+        },
+        which_key = true,
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      },
+    })
 
-		-- setup must be called before loading
-		vim.cmd.colorscheme("catppuccin")
-	end,
+    -- setup must be called before loading
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
-
--- NOTE: tokyonight
---
--- return {
--- 	"folke/tokyonight.nvim",
--- 	priority = 1000,
--- 	init = function()
--- 		-- Load the colorscheme here.
--- 		-- Like many other themes, this one has different styles, and you could load
--- 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
--- 		vim.cmd.colorscheme("tokyonight-night")
---
--- 		-- You can configure highlights by doing something like:
--- 		vim.cmd.hi("Comment gui=none")
--- 	end,
--- }
